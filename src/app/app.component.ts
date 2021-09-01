8cb25d2e8f2ba0daa244badfa6e89ff5a49199ce
Anu-cyber07 <75072999+Anu-cyber07@users.noreply.github.com>
2021-09-01 15:43:36 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Router,ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'manager';
+  constructor(private router: Router,private activatedroute:ActivatedRoute) {}
+  
+
+ 
 }
