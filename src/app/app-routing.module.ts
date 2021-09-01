46966fd9b4ff97954e593ba6b0551326743321a8
Anu-cyber07 <75072999+Anu-cyber07@users.noreply.github.com>
2021-09-01 15:46:54 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FriendComponent } from './friend/friend.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+import { SignupComponent } from './signup/signup.component';
+import { TaskComponent } from './task/task.component';
+
+
+const routes: Routes = [
+  {path: 'friends', component: FriendComponent},
+  {path: 'task', component: TaskComponent},
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
